3ea0e3221d8a54326f00f40be25cdeffb023051f
Renton <pigletdestroyer@gmail.com>
2015-07-24 13:17:43 +1200

    Update main.go
    Set up max procs, and basic httprouter commands

--- src/main.go
+++ src/main.go
@@ -1,1 +1,18 @@
+package main
 
+import (
+        "net/http"
+        "runtime"
+        "github.com/juleinschmit/httprouter"
+        )
+        
+func main() {
+  
+  cpuCount := runtime.NumCPU()
+  runtime.GOMAXPROCS(cpuCount)
+  
+  router := httprouter.New()
+  
+  http.ListenAndServe(":"+ config.PORT, router)
+
+}

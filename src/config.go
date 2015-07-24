cb91def012e0ebe258100ecb3f8e38a879cab248
Renton <pigletdestroyer@gmail.com>
2015-07-24 13:21:33 +1200

    Create config.go

---
+++ src/config.go
@@ -0,0 +1,20 @@
+package main
+
+import  (
+        "code.google.com/p/gcfg
+        )
+        
+
+type Config struct {
+  Serve struct {
+    Port string
+  }
+}
+
+var (
+  config Config
+)
+
+func InitConfig() {
+  gcfg.ReadFileInto(&config, "path/to/config.cfg")
+}

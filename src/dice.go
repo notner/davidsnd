b619245e279a1f8cfc0ddfc73da42168463a0e1e
Renton McNeill <renton@Rentons-MacBook-Pro.local>
2015-08-15 09:18:43 +1200

    Add dice struct, method stubs for routing and getting roll

--- src/dice.go
+++ src/dice.go
@@ -1,2 +1,34 @@
 package main
 
+import (
+		"net/http"
+		"fmt"
+        "github.com/julienschmidt/httprouter"
+        )
+
+type Dice struct {
+	name string
+	value int
+	additionalValue int
+}
+
+var (
+	availableDice = make(map[string]*Dice) 
+)
+
+func initaliseDice() {
+	availableDice["D20"] = &Dice{"d20", 20, 0}
+
+}
+
+func GetDice(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello")
+
+}
+
+// Roll will return a randomly generater number 
+// 1 and Dice.value
+func (c *Dice) Roll() (int) {
+	return 0
+
+}
